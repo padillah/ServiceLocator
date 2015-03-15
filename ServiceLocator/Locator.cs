16f078d7a887a4f68a7c9981c3044f19674c2bbc
Henry Padilla <padilla.henry@gmail.com>
2015-03-15 11:19:04 -0500

    Initial files.

---
+++ ServiceLocator/Locator.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace ServiceLocator
+{
+    /// <summary>
+    /// A very simple service locator.
+    /// </summary>
+    public static class Locator
+    {
+        private static Dictionary<Type, ServiceInfo> services = new Dictionary<Type, ServiceInfo>();
+
+        /// <summary>
+        /// Registers a service.
+        /// </summary>
+        public static void Register<TInterface, TImplemention>() where TImplemention : TInterface
+        {
+            Register<TInterface, TImplemention>(false);
+        }
+
+
+        /// <summary>
+        /// Registers a service as a singleton.
+        /// </summary>
+        public static void RegisterSingleton<TInterface, TImplemention>() where TImplemention : TInterface
+        {
+            Register<TInterface, TImplemention>(true);
+        }
+
+
+        /// <summary>
+        /// Resolves a service.
+        /// </summary>
+        public static TInterface Resolve<TInterface>()
+        {
+            try
+            {
+                return (TInterface) services[typeof (TInterface)].ServiceImplementation;
+            }
+            catch (KeyNotFoundException ex)
+            {
+                throw new ApplicationException(typeof(TInterface).Name + " has not been added to the Locator Service.", ex);
+            }
+        }
+
+        /**/
+
+        public static void Reset()
+        {
+            services.Clear();
+        }
+
+        public static void Register<TInterface, TImplemention>(TImplemention argImplementation) where TImplemention : TInterface
+        {
+            ServiceInfo localInfo = new ServiceInfo(typeof(TImplemention), true, argImplementation);
+
+            services.Add(typeof(TInterface), localInfo);
+        }
+
+        /**/
+
+        /// <summary>
+        /// Registers a service.
+        /// </summary>
+        /// <param name="argIsSingleton">true if service is Singleton; otherwise false.</param>
+        private static void Register<TInterface, TImplemention>(bool argIsSingleton) where TImplemention : TInterface
+        {
+            services.Add(typeof(TInterface), new ServiceInfo(typeof(TImplemention), argIsSingleton));
+        }
+
+
+        /// <summary>
+        /// Class holding service information.
+        /// </summary>
+        class ServiceInfo
+        {
+            private Type _serviceImplementationType;
+            private object _serviceImplementation;
+            private bool isSingleton;
+
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="ServiceInfo"/> class.
+            /// </summary>
+            /// <param name="argServiceImplementationType">Type of the service implementation.</param>
+            /// <param name="argIsSingleton">Whether the service is a Singleton.</param>
+            public ServiceInfo(Type argServiceImplementationType, bool argIsSingleton)
+            {
+                _serviceImplementationType = argServiceImplementationType;
+                isSingleton = argIsSingleton;
+            }
+
+            public ServiceInfo(Type argServiceImplementationType, bool argIsSingleton, object argConcreteClass)
+                : this(argServiceImplementationType, argIsSingleton)
+            {
+                _serviceImplementation = argConcreteClass;
+            }
+
+            /// <summary>
+            /// Gets the service implementation.
+            /// </summary>
+            public object ServiceImplementation
+            {
+                get
+                {
+                    if (isSingleton)
+                    {
+                        if (_serviceImplementation == null)
+                        {
+                            _serviceImplementation = CreateInstance(_serviceImplementationType);
+                        }
+
+                        return _serviceImplementation;
+                    }
+
+                    return CreateInstance(_serviceImplementationType);
+                }
+            }
+
+
+            /// <summary>
+            /// Creates an instance of a specific type.
+            /// </summary>
+            /// <param name="argType">The type of the instance to create.</param>
+            private static object CreateInstance(Type argType)
+            {
+                if (services.ContainsKey(argType))
+                {
+                    return services[argType].ServiceImplementation;
+                }
+
+                ConstructorInfo ctor = argType.GetConstructors().First();
+
+                var parameters =
+                    from parameter in ctor.GetParameters()
+                    select CreateInstance(parameter.ParameterType);
+
+                return Activator.CreateInstance(argType, parameters.ToArray());
+            }
+        }
+    
+    }
+
+}
---
+++ ServiceLocator/WindowViewModelMapping/WindowViewModelMappings.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using ServiceLocator.Annotations;
+
+namespace ServiceLocator.WindowViewModelMapping
+{
+    /// <summary>
+    /// Class describing the Window-ViewModel mappings used by the dialog service.
+    /// </summary>
+    public abstract class WindowViewModelMappings : IWindowViewModelMappings
+    {
+        protected IDictionary<Type, Type> Mappings { get; set; }
+
+        /// <summary>
+        /// Gets the window type based on registered ViewModel type.
+        /// </summary>
+        /// <param name="argViewModelType">The type of the ViewModel.</param>
+        /// <returns>The window type based on registered ViewModel type.</returns>
+        public virtual Type GetWindowTypeFromViewModelType(Type argViewModelType)
+        {
+            return Mappings[argViewModelType];
+        }
+
+        ///// <summary>
+        ///// Initializes a new instance of the <see cref="WindowViewModelMappings"/> class.
+        ///// </summary>
+        //public WindowViewModelMappings()
+        //{
+        //    mappings = new Dictionary<Type, Type>
+        //    {
+        //        { typeof(MainViewModel), typeof(string) },
+        //        { typeof(NewProjectViewModel), typeof(NewProjectView) },
+        //        { typeof(EditProjectViewModel), typeof(NewProjectView) },
+        //        { typeof(NewBacklogViewModel), typeof(NewBacklogView) },
+        //        { typeof(EditBacklogViewModel), typeof(EditBacklogView) }
+
+        //    };
+        //}
+
+
+
+    }
+
+}
